ef6c5d278e7af010bfcc8926d0681aa2392d2f6e
doniwirawan <doniwirawan166@gmail.com>
2021-09-28 09:38:27 +0800

    adding maps

---
+++ resources/views/includes/maps.blade.php
@@ -0,0 +1,17 @@
+
+{{-- cta --}}
+<section class="call-to-action p-5 mt-5 bg-maps">
+    <div class="container p-4">
+        <div class="row d-flex justify-content-center">
+            <div class="col-12">
+                <h2 class="title-a text-center">Maps</h2>
+                <p class="text-center text-white">for those of you who prefer a map view, we have it for you.</p>
+            </div>
+            <div class="col-12 d-flex justify-content-center" id="map">
+                <a href="/maps"
+                    class="btn btn-primary  d-flex align-items-center justify-content-center mx-1 p-2 h5">Check Our
+                    Maps</a>
+            </div>
+        </div>
+    </div>
+</section>
