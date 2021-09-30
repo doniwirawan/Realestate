9f1427d0cc4228372682644cf3a10ef7230951e1
doniwirawan <doniwirawan166@gmail.com>
2021-09-30 09:51:54 +0800

    adding ceo about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -45,7 +45,7 @@ About
             <div class="col-md-12 section-t8">
                 <div class="row">
                     <div class="col-md-6 col-lg-5">
-                        <img src="frontend/img/about-2.jpg" alt="" class="img-fluid">
+                        <img src="frontend/img/ceo_indonesiapropertyhub.jpeg" alt="" class="img-fluid">
                     </div>
                     <div class="col-lg-2  d-none d-lg-block">
                         <div class="title-vertical d-flex justify-content-start">
@@ -55,23 +55,22 @@ About
                     <div class="col-md-6 col-lg-5 section-md-t3">
                         <div class="title-box-d">
                             <h3 class="title-d">Pierre
-                                <span class="color-d">Masnada</span> 
+                                <span class="color-d">Masnada</span>
                                 <br></h3>
                         </div>
                         <p class="color-text-a">
-                            Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia
-                            eget
-                            consectetur sed, convallis
-                            at tellus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum
-                            ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit
-                            neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.
-                        </p>
-                        <p class="color-text-a">
-                            Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget consectetur sed, convallis at
-                            tellus.
-                            Mauris blandit aliquet
-                            elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed,
-                            convallis at tellus.
+                            Now CEO of Indonesia Property Hub, Pierre-Alain Masnada studied Business in
+                            the USA before running companies in the yachting space for
+                            10 years.
+                            <br>
+                            <br>
+                            For the last 5 years, along with his team, he has been
+                            specializing in materializing real estate projects in Bali and its
+                            surrounding islands, valued at more than 13 million USD.
+                            <br>
+                            <br>
+                            His fortes lie in the creation and development of high quality,
+                            high return projects.
                         </p>
                     </div>
                 </div>
