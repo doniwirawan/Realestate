19ec4d0dcc3d3d0fd6c9db1da7b1b79358a1d863
doniwirawan <doniwirawan166@gmail.com>
2021-09-30 14:50:51 +0800

    change the css of admin

--- resources/views/includes/admin/header.blade.php
+++ resources/views/includes/admin/header.blade.php
@@ -1,6 +1,5 @@
 <header class="header" id="header">
     <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
-    <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
     <ul class="navbar-nav ml-auto">
         <div class="d-flex">
             <li class="nav-item dropdown">
